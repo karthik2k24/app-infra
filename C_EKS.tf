48d5f958f3a92b37ccdbe5db8114a0d179a142d6
karthik M <karthik@karthiks-MacBook-Air.local>
2025-02-03 00:13:43 +0530

    test

--- C_EKS.tf
+++ C_EKS.tf
@@ -16,12 +16,42 @@ resource "aws_iam_role" "eks_cluster_role" {
     ]
   })
 }
+######
+  # resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  #   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  #   role       = aws_iam_role.master.name
+  # }
+
+  # resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+  #   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  #   role       = aws_iam_role.master.name
+  # }
+
+  # resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
+  #   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  #   role       = aws_iam_role.master.name
+  # }
+
+#######
+
+
 
 # IAM Policy for EKS Cluster Role
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy_attachment" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+
+  resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+    role       = aws_iam_role.eks_cluster_role.name
+  }
+
 resource "aws_iam_role_policy_attachment" "eks_node_cni_policy_attachment" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
@@ -52,11 +82,50 @@ resource "aws_iam_role_policy_attachment" "eks_node_policy_attachment" {
 }
 
 
+
 resource "aws_iam_role_policy_attachment" "eks_node_ecr_policy_attachment" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+
+##############
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+    role       = aws_iam_role.eks_node_role.name
+  }
+
+  resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+    role       = aws_iam_role.eks_node_role.name
+  }
+
+  resource "aws_iam_role_policy_attachment" "AmazonSSMManagedInstanceCore" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    role       = aws_iam_role.eks_node_role.name
+  }
+
+  resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+    role       = aws_iam_role.eks_node_role.name
+  }
+
+  resource "aws_iam_role_policy_attachment" "s3" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+    role       = aws_iam_role.woreks_node_roleker.name
+  }
+
+  resource "aws_iam_role_policy_attachment" "autoscaler" {
+    policy_arn = aws_iam_policy.autoscaler.arn
+    role       = aws_iam_role.eks_node_role.name
+  }
+
+  # resource "aws_iam_instance_profile" "worker" {
+  #   depends_on = [aws_iam_role.eks_node_role]
+  #   name       = "veera-eks-worker-new-profile"
+  #   role       = aws_iam_role.eks_node_role.name
+  # }
+##############
 
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "pvt-app-cluster"
