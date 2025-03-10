3e023f0739a83de6bd85e2ac55d4636645f29d7d
karthik <karthik@local>
2025-03-10 18:19:09 +0530

    instance tyepe medium

--- eks.tf
+++ eks.tf
@@ -32,7 +32,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   update_config {
     max_unavailable = 1
   }
-  instance_types = ["t2.medium"]
+  instance_types = ["t2.large"]
   capacity_type  = "SPOT"
 
   launch_template {
