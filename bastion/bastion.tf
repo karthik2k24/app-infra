ebe1f7dbbf7aef9c489f3a82751bebd4346bff6a
karthik <karthik@local>
2025-03-12 18:28:08 +0530

    move bastion sg

--- bastion/bastion.tf
+++ bastion/bastion.tf
@@ -1,12 +1,12 @@
-# #EC2 Instance with Bastion Security Group
-# resource "aws_instance" "bastion_instance" {
-#   ami                    = "ami-0226f5d1e7c6fc56e"
-#   instance_type          = "t2.micro"               # Choose your preferred instance type
-#   key_name               = "SRE2"                   # Replace with your SSH key pair name
-#   subnet_id              = aws_subnet.pub_app_1a.id # Replace with the appropriate public subnet ID 
-#   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+#EC2 Instance with Bastion Security Group
+resource "aws_instance" "bastion_instance" {
+  ami                    = "ami-0226f5d1e7c6fc56e"
+  instance_type          = "t2.micro"               # Choose your preferred instance type
+  key_name               = "SRE2"                   # Replace with your SSH key pair name
+  subnet_id              = aws_subnet.pub_app_1a.id # Replace with the appropriate public subnet ID 
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
-#   tags = {
-#     Name = "Bastion Host"
-#   }
-# }
+  tags = {
+    Name = "Bastion Host"
+  }
+}
