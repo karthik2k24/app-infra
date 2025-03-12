0ba7355bb8c148fd428758621142e17f0ceebb94
karthik <karthik@local>
2025-03-12 18:58:00 +0530

    added 2 pub-subnets

--- vpc.tf
+++ vpc.tf
@@ -21,28 +21,22 @@ resource "aws_subnet" "pub_app_1a" {
   }
 }
 
-
-
-
 resource "aws_subnet" "pub_app_1d" {
   vpc_id                  = aws_vpc.pvt_app_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1d"
   map_public_ip_on_launch = true
-tags = {
+  tags = {
     Name                             = "pub-app-1d"
     "kubernetes.io/role/elb"         = "1"  # Required for internet-facing ALB
     "kubernetes.io/cluster/pvt-app-cluster" = "owned"  # Ensures EKS can use it
   }
 }
 
-
 resource "aws_subnet" "pvt_app_1b" {
   vpc_id            = aws_vpc.pvt_app_vpc.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1b"
-
-
   tags = {
     Name = "pvt-app-1b"
     "kubernetes.io/role/internal-elb"         = "1"  # Required for internal ALB
--- vpc_nat.tf
+++ vpc_nat.tf
@@ -1,40 +1,75 @@
-# Elastic IP for NAT Gateway
-resource "aws_eip" "nat_eip" {
+# Elastic IP for NAT Gateway in pub_app_1a
+resource "aws_eip" "nat_eip_1a" {
+  vpc = true
   tags = {
-    Name = "nat-eip"
+    Name = "nat-eip-1a"
   }
 }
-# NAT Gateway
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.pub_app_1a.id # Ensure this is in the public subnet
 
+# NAT Gateway in pub_app_1a
+resource "aws_nat_gateway" "nat_gw_1a" {
+  allocation_id = aws_eip.nat_eip_1a.id
+  subnet_id     = aws_subnet.pub_app_1a.id
   tags = {
-    Name = "nat-gateway"
+    Name = "nat-gateway-1a"
   }
 }
-# Route for Private Subnets to access the internet via NAT Gateway
-resource "aws_route" "private_route" {
-  route_table_id         = aws_route_table.pvt_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gw.id
+
+# Elastic IP for NAT Gateway in pub_app_1d
+resource "aws_eip" "nat_eip_1d" {
+  vpc = true
+  tags = {
+    Name = "nat-eip-1d"
+  }
 }
 
+# NAT Gateway in pub_app_1d
+resource "aws_nat_gateway" "nat_gw_1d" {
+  allocation_id = aws_eip.nat_eip_1d.id
+  subnet_id     = aws_subnet.pub_app_1d.id
+  tags = {
+    Name = "nat-gateway-1d"
+  }
+}
 
-resource "aws_route_table" "pvt_rt" {
+# Private Route Table for 1a
+resource "aws_route_table" "private_rt_1a" {
   vpc_id = aws_vpc.pvt_app_vpc.id
   tags = {
-    Name = "pvt-rt"
+    Name = "private-rt-1a"
   }
 }
 
-
-resource "aws_route_table_association" "pvt_app_1b_association" {
-  subnet_id      = aws_subnet.pvt_app_1b.id
-  route_table_id = aws_route_table.pvt_rt.id
+# Route for Private Subnets in 1a to access the internet via NAT Gateway
+resource "aws_route" "private_route_1a" {
+  route_table_id         = aws_route_table.private_rt_1a.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw_1a.id
 }
 
+# Private Route Table for 1d
+resource "aws_route_table" "private_rt_1d" {
+  vpc_id = aws_vpc.pvt_app_vpc.id
+  tags = {
+    Name = "private-rt-1d"
+  }
+}
+
+# Route for Private Subnets in 1d to access the internet via NAT Gateway
+resource "aws_route" "private_route_1d" {
+  route_table_id         = aws_route_table.private_rt_1d.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw_1d.id
+}
+
+# Associate Private Subnet 1b with Route Table 1a
+resource "aws_route_table_association" "pvt_app_1b_association" {
+  subnet_id      = aws_subnet.pvt_app_1b.id
+  route_table_id = aws_route_table.private_rt_1a.id
+}
+
+# Associate Private Subnet 1c with Route Table 1d
 resource "aws_route_table_association" "pvt_app_1c_association" {
   subnet_id      = aws_subnet.pvt_app_1c.id
-  route_table_id = aws_route_table.pvt_rt.id
+  route_table_id = aws_route_table.private_rt_1d.id
 }
